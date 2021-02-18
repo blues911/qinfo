50a442da92a853b8bfeabbc651ef5b6c1305d47e
smokehill <valera.padolochniy@gmail.com>
2021-02-18 22:10:01 +0300

    sudo usage

--- sh/install.sh
+++ sh/install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# qinfo install script
 
 if [ -d /usr/local/qinfo ]; then
     echo "qinfo is already installed"
@@ -8,34 +7,9 @@ fi
 
 fail=0
 
-if [[ "$(whoami)" != "root" ]]; then
-    fail=$((fail + 1))
-    echo "Error: permission denied (run this script as root)"
-fi
-
 if [[ "$(uname)" != "Linux" ]]; then
     fail=$((fail + 1))
     echo "Error: qinfo works only on Linux"
-fi
-
-if [[ "$(which python)" == "" ]]; then
-    fail=$((fail + 1))
-    echo "Error: python not found"
-fi
-
-if [[ "$(which free)" == "" ]]; then
-    fail=$((fail + 1))
-    echo "Error: free not found"
-fi
-
-if [[ "$(which df)" == "" ]]; then
-    fail=$((fail + 1))
-    echo "Error: df not found"
-fi
-
-if [[ "$(which uptime)" == "" ]]; then
-    fail=$((fail + 1))
-    echo "Error: uptime not found"
 fi
 
 if [ ! -d ~/.cache ]; then
@@ -48,6 +22,9 @@ if [[ $fail > 0 ]]; then
 fi
 
 echo "Installing..."
-cp -r ./qinfo /usr/local/qinfo
-cp ./sh/qinfo /usr/local/bin/qinfo
+# install system deps
+sudo apt-get install -y build-essential procps python
+# install qinfo
+sudo cp -r ./qinfo /usr/local/qinfo
+sudo cp ./sh/qinfo /usr/local/bin/qinfo
 echo "Ok"
--- sh/uninstall.sh
+++ sh/uninstall.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-# qinfo uninstall script
 
 if [ ! -d /usr/local/qinfo ]; then
     echo "qinfo is not installed"
     exit 0
 fi
 
-if [[ "$(whoami)" != "root" ]]; then
-    echo "Error: permission denied (run this script as root)"
-    exit 1
-fi
-
 echo "Uninstalling..."
-rm -f /usr/local/bin/qinfo
-rm -rf /usr/local/qinfo
-rm -rf ~/.cache/qinfo
+# uninstall qinfo
+sudo rm -f /usr/local/bin/qinfo
+sudo rm -rf /usr/local/qinfo
+sudo rm -rf ~/.cache/qinfo
 echo "Ok"
